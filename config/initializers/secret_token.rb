66d227b20f3a7d5c9c54f7b3144fe3d65f6114ed
Pikaj <pikajpij@gmail.com>
2013-12-02 21:20:53 +0100

    empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoEat::Application.config.secret_key_base = '55094bae26ed1c329bae7117062d541d8c5edcff5fe975dc7d4827c42090b124adfdda0672f500b26d6fdf76405f9644fa12df49f65096115daeda1393c109e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoEat::Application.config.session_store :cookie_store, key: '_GoEat_session'
